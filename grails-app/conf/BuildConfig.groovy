5759fbba410a000a325cad362435376f1d3ddb09
Dave Martin <djtfmartin@gmail.com>
2015-07-31 14:17:37 +0100

    use latest plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build ":release:3.0.1"
-        runtime ":collectory:1.4.1"
+        runtime ":collectory:1.4.2"
         build ":tomcat:7.0.54"
     }
 }
